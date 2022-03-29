18e2ba59c15286550cd52b8bb98c6f7358c13b92
zxdcsg <78397426+zxdcsg@users.noreply.github.com>
2022-03-29 14:14:26 +0800

    攻擊系統(未完_1)

---
+++ 40725053_220222/Assets/Scripts/DataWeapon.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+    
+namespace Oliya
+{
+    /// <summary>
+    ///武器資料
+    ///1.飛行速度 float
+    ///2.攻擊力 float
+    ///3.起始數量 int
+    ///4.最大數量 int
+    ///5.間格時間 float
+    ///6.生成位置
+    ///</summary>
+    //ScriptableObject 腳本化物件 
+    //作用:將腳本的資料變成物件儲存於 Project內(擴充與維護性佳)
+    //CreateAssetMenu與ScriptableObject搭配使用
+    //作用:在Project建立此腳本化物件的選單與檔案名稱
+    //menuName 選單名稱 fileName 檔案名稱
+    [CreateAssetMenu(menuName = "Oliya/Data Weapon", fileName = "Data Weapon")]
+    public class DataWeapon : ScriptableObject
+    {
+        [Header("飛行速度"), Range(0, 5000)]
+        public float speed = 500;
+        [Header("攻擊力"), Range(0, 100)]
+        public float attack = 10;
+        [Header("起始數量"), Range(1, 10)]
+        public int countstart = 1;
+        [Header("最大數量"), Range(1, 20)]
+        public int countMax = 3;
+        [Header("間格時間"), Range(0, 5)]
+        public float interval = 3.5f;
+    }
+}
